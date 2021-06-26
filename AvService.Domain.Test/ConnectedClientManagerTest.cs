cfb2dc2cb19d2582c169eb908cf543f7802de189
Andrei Mihai <mihai.andrei@yahoo.com>
2021-06-26 12:04:52 +0300

    Add test for connections

---
+++ AvService.Domain.Test/ConnectedClientManagerTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace AvService.Domain.Test
+{
+    public class ConnectedClientManagerTest
+    {
+        ConnectedClientManager connectedClientManager;
+
+        [SetUp]
+        public void Setup()
+        {
+            connectedClientManager = new ConnectedClientManager();
+        }
+
+        [Test]
+        public void WhenAClientIsConnectedThenNoOtherClientsCanConnect()
+        {
+            connectedClientManager.Connect();
+            Assert.IsFalse(connectedClientManager.Connect());
+        }
+
+        [Test]
+        public void WhenAClientIDisconnectsThenOtherClientsCanConnect()
+        {
+            connectedClientManager.Connect();
+            connectedClientManager.Disconect();
+            Assert.IsTrue(connectedClientManager.Connect());
+        }
+    }
+}
